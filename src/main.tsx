7ce740325209f1c424eb85c0ab2ae1678bd8fb58
Robert Gabriel <robertdsgabriel@gmail.com>
2026-09-17 14:21:45 -0300

    feat: initialize versioned storage and responsive styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { migrateLegacyStorage } from './persistence'
+import './styles/responsive-v2.css'
+
+migrateLegacyStorage()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
